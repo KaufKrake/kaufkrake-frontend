ae549927f115209219724775fe3d1f7c5110a43c
Dominic Matte <mattedominic@gmx.de>
2023-06-04 19:17:33 +0200

    refactor: export default Profil to Profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Profil = () => {
+export const Profile = () => {
   return (
     <div className="text-red-800">PROFIL</div>
   )
 }
 
-export default Profil
+export default Profile
